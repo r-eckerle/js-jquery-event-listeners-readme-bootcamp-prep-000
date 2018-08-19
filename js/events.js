33a3657badee2a871ce4d0eff90946a8d5a07475
Ryan <eckerle_r@protonmail.com>
2018-08-19 17:43:09 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,19 @@
 //define functions here
 
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!");
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('.tasty');
+  });
+}
+
 $(document).ready(function(){
 
 // call functions here
-
+  getIt();
 });
