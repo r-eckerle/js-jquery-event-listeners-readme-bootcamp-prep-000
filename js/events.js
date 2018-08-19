174b73301deb476512c876170381b8d6a18c9873
Ryan <eckerle_r@protonmail.com>
2018-08-19 17:52:28 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -22,7 +22,7 @@ function pressIt() {
 
 function submitIt() {
   $('form').on('submit', function() {
-    alert('Your form is being submitted now')
+    alert('Your form is going to be submitted now')
   })
 }
 
